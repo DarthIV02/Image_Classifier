e46f8071d60610b6d452a6b993a49cd9b190b32c
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-07-31 16:20:43 -0700

    ddcolor

--- ddcolor/script.py
+++ ddcolor/script.py
@@ -5,7 +5,7 @@ from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 
 img_colorization = pipeline(Tasks.image_colorization, model='damo/cv_ddcolor_image-colorization')
-for f in os.listdir('./landscape_256x256_bw/'):
-    result = img_colorization(f'./landscape_256x256_bw/{f}')
+for f in os.listdir('./imagenet_64x64_bw/'):
+    result = img_colorization(f'./imagenet_64x64_bw/{f}')
     cv2.imwrite(f'output/{f}', result[OutputKeys.OUTPUT_IMG])
     print(f)
